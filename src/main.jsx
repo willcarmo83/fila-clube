deee289228787e1b88eb782fd0a7a0fa6f7b4c91
willcarmo83 <william.s.carmo83@gmail.com>
2026-08-18 17:27:07 -0300

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <div style={{ maxWidth: "760px", margin: "0 auto", padding: "24px 16px" }}>
+    <div style={{ maxWidth: "1180px", margin: "0 auto", padding: "24px 16px" }}>
       <App />
     </div>
   </React.StrictMode>
